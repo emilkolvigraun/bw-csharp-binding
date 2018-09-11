428cbd1a94b5269946b8974a5600644c13f618ec
Emil Stubbe Kolvig-Raun <emilkolvigraun@gmail.com>
2018-09-11 21:37:19 +0200

    Push:Bottomline

---
+++ BWBinding/BWBinding/BossWaveClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BWBinding
+{
+    /**
+     *  Aggregator class
+     *  @Author: Emil Stubbe Kolvig-Raun
+     *  
+     *  Implements IDisposable, c# equivalent to Java's AutoClosable
+    **/
+    public class BossWaveClient : IDisposable
+    {
+        private string hostName;
+        private int portNumber;
+
+        public BossWaveClient(String hostName, int portNumber)
+        {
+            this.hostName = hostName;
+            this.portNumber = portNumber;
+            // Declare other logic.
+        }
+        
+        public void Dispose()
+        {
+            // Dispose of all components. Close the socket and the stream.
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# bw-csharp-binding
-An implementation of the c# binding for BOSSWAVE.
+# BOSSWAVE Binding
+## C# Base Class Library
+**This is a** <span style="color:purple">**.NET Framework**</span> **manifesting the BOSSWAVE 2.2.0 bindings for** <span style="color:purple">**C#**</span>**.**
+<br>
+*University of Southern Denmark*, Center for Energy Informatics.
+
+### What's new
+```
+First commit
+```
+
+## Instructions
+First of all you have to install the latest release of BOSSWAVE.
+<br><br>
+**Install on Windows:**
+<br>
+```
+instruction
+```
+<br>
+<br>
+**Install on Ubuntu:**
+<br>
+```
+curl get.bw2.io/agent | sudo bash
+```
