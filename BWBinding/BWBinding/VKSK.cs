2ccbeaae11834599eb45ecf357180fbf9fba9c21
unknown <emilkolvigraun@gmail.com>
2018-09-17 15:22:15 +0200

    git ignore

---
+++ BWBinding/BWBinding/VKSK.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BWBinding
+{
+    class VKSK
+    {
+        private String key { get; }
+        private byte[] value { get; }
+
+        public VKSK(String key, byte[] value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+
+        public void writeToStream(StreamWriter outputStream)
+        {
+            String header = String.Format("kv %s %d\n", key, this.value.Length);
+            outputStream.Write(header);
+            outputStream.Write(this.value);
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -12,7 +12,7 @@ First commit
 ## Instructions
 First of all you have to install the latest release of BOSSWAVE.
 
-**Install on Ubuntu:**
+**Install on Ubuntu:** <br>
 If you're on windows use [WSL](https://www.howtogeek.com/249966/how-to-install-and-use-the-linux-bash-shell-on-windows-10/).
 ```
 curl get.bw2.io/agent | sudo bash
