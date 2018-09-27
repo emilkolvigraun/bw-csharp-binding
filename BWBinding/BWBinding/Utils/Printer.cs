ffc253c23deb6365b1a05d8f125a15970715976f
wizardmil <emilkolvigraun@gmail.com>
2018-09-27 10:34:44 +0200

    Added Printer to lib

--- BWBinding/BWBinding/Utils/Printer.cs
+++ BWBinding/BWBinding/Utils/Printer.cs
@@ -1,6 +1,7 @@
 ﻿using BWBinding.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,134 @@ namespace BWBinding.Utils
 {
     public static class Printer
     {
-        public static void ConstructMessage()
+        public static string ConstructMessage(Message message)
         {
+            string f_wall = "";
+            string s_wall = "";
+            string t_wall = ""; 
+            string fo_wall = "";
+            
+            string roof = " ______________________________________________________________________________";
+            string roof1 = "\n|           |                                                                  |\n";
+            string bottom = "\n            |__________________________________________________________________|";
 
+            string uri = message.uri;
+            string from = message.from;
+            string type = message.payloadObjects[0].type.ToString();
+            string load = Encoding.UTF8.GetString(message.payloadObjects[0].load);
+
+            if (uri.Length <= 59)
+            {
+                int count = 59 - uri.Length;
+                for (int i = 0; i < count; i++)
+                {
+                    f_wall += " ";
+                }
+
+                f_wall += "|";
+            }
+            else
+            {
+                uri = BreakTillFit(uri, 53);
+            }
+
+            if (from.Length <= 58)
+            {
+                int count = 58 - from.Length;
+                for (int i = 0; i < count; i++)
+                {
+                    s_wall += " ";
+                }
+
+                s_wall += "|";
+            }
+            else
+            {
+                from = BreakTillFit(from, 53);
+            }
+
+            if (type.Length <= 58)
+            {
+                int count = 58 - type.Length;
+                for (int i = 0; i < count; i++)
+                {
+                    t_wall += " ";
+                }
+
+                t_wall += "|";
+            }
+            else
+            {
+                type = BreakTillFit(type, 53);
+            }
+
+            if (load.Length <= 55)
+            {
+                int count = 55 - load.Length;
+                for (int i = 0; i < count; i++)
+                {
+                    fo_wall += " ";
+                }
+
+                fo_wall += "|";
+            }
+            else
+            {
+                load = BreakTillFit(load, 53);
+            }
+            return roof + roof1 + "|  Message  |  uri: " + uri + f_wall + "\n|___________|  from: " + from + s_wall + "\n            |  type: " + type + t_wall + "\n            |  message: " + load + fo_wall + bottom;
+        }
+
+        private static string AddEnd(int length, int next, string concerns)
+        {
+            int count = next - length;
+            string temp = "";
+            for (int i = 0; i < count; i++)
+            {
+                temp += " ";
+            }
+
+            temp += "|";
+            return concerns.Insert(length, temp);
+        }
+
+        private static string BreakTillFit(string con, int first_divider)
+        {
+            string str = con;
+
+            if (str[first_divider].Equals(' '))
+            {
+                str = str.Insert(first_divider, "  |\n            | ");
+            }
+            else
+            {
+                str = str.Insert(first_divider, "  |\n            |  ");
+            } 
+            int length = str.Length;
+            int itt = 1;
+            int ideal_length = 134;
+            for (int i = 0; i < itt; i++)
+            {
+                if (length > ideal_length - 79)
+                {
+
+
+                    if (str[ideal_length].Equals(' '))
+                    {
+                        str = str.Insert(ideal_length, "  |\n            | ");
+                    }
+                    else
+                    {
+                        str = str.Insert(ideal_length, "  |\n            |  ");
+                    }
+                    ideal_length += 81;
+                    itt += 1;
+                }
+            }
+
+            str = AddEnd(str.Length, ideal_length + 2, str);
+
+            return str;
         }
 
         public static string ConstructResponse(RequestType reason, string status)
@@ -29,6 +155,16 @@ namespace BWBinding.Utils
                 wall += "|";
             }
             return " _________________________ " + "\n|          |              |" + "\n| Response | status: " + status + " |" + "\n|__________| " + reason.ToString() + wall + "\n           |______________|"; ;
+        }
+
+        public static void PrintResponse(RequestType reason, string status)
+        {
+            Console.WriteLine(ConstructResponse(reason, status));
+        }
+
+        public static void PrintMessage(Message message)
+        {
+            Console.WriteLine(ConstructMessage(message));
         }
     }
 }
--- BWBinding/Bootstrap/ClientImplementation.cs
+++ BWBinding/Bootstrap/ClientImplementation.cs
@@ -10,43 +10,43 @@ namespace Bootstrap
 {
     class ClientImplementation
     {
+        private static Semaphore semaphore;
+        private static IResponseHandler subscribeHandler;
+        private static IResponseHandler publishHandler;
+        private static IResponseHandler publishHandler1;
+        private static IResponseHandler publishHandler2;
+        private static IMessageHandler nsgHandler;
+
         static void Main(string[] args)
         {
-            Console.WriteLine(Printer.ConstructResponse(RequestType.MAKE_ENTITY, "okay"));
-            Console.WriteLine(Printer.ConstructResponse(RequestType.SUBSCRIBE, "okay"));
-            Console.WriteLine(Printer.ConstructResponse(RequestType.MAKE_DOT, "okay"));
-            Console.WriteLine(Printer.ConstructResponse(RequestType.PUBLISH, "okay"));
-            
+
+            semaphore = new Semaphore(0, 1);
+            subscribeHandler = new ResponseHandler();
+            publishHandler = new ResponseHandler();
+            publishHandler1 = new ResponseHandler();
+            publishHandler2 = new ResponseHandler();
+            nsgHandler = new MessageHandler();
+
+            test();
         }
 
-        private void test()
+        private static void test()
         {
+
             BossWaveClient bwClient = new BossWaveClient("localhost", BWDefaults.DEFAULT_PORT_NUMBER);
 
             bwClient.Connect();
 
             bwClient.SetEntity("C:/Users/Emil S. Kolvig-Raun/stubbe.ent", BWDefaults.DEFAULT_RESPONSEHANDLER(1));
 
-
             Request subscribeRequest = new RequestUtils("stubbe.demo.dk/sharptesting", RequestType.SUBSCRIBE)
                 .SetExpiryDelta(3600000)
                 .SetAutoChain(true)
                 .BuildSubcriber();
 
-            bwClient.Subscribe(subscribeRequest, BWDefaults.DEFAULT_RESPONSEHANDLER(2), BWDefaults.DEFAULT_MESSAGEHANDLER(2));
+            bwClient.Subscribe(subscribeRequest, subscribeHandler, nsgHandler);
 
-            if (!BWDefaults.DEFAULT_RESPONSEHANDLER(2).received)
-            {
-                Console.Write("Working"); // This looks awesome
-                Thread.SpinWait(6666666); //Wait for subscribe to finish, before publishing.
-                Console.Write(".");
-                Thread.SpinWait(6666666); //Wait for subscribe to finish, before publishing.
-                Console.Write(".");
-                Thread.SpinWait(6666666); //Wait for subscribe to finish, before publishing.
-                Console.WriteLine(".");
-
-            }
-
+            semaphore.WaitOne();
             RequestUtils publishRequestUtils = new RequestUtils("stubbe.demo.dk/sharptesting", RequestType.PUBLISH)
                 .SetPrimaryAccessChain("klAVQ9UOajySAZZWzdU4IBrdyqs85JOk3TJhnpecFqg=");
             byte[] message = Encoding.UTF8.GetBytes("Rest is for the wicked.");
@@ -54,32 +54,27 @@ namespace Bootstrap
             PayloadObject payload = new PayloadObject(new PayloadType(text), message);
             publishRequestUtils.AddPayloadObject(payload);
             Request publishRequest = publishRequestUtils.BuildPublisher();
-            bwClient.Publish(publishRequest, BWDefaults.DEFAULT_RESPONSEHANDLER(3));
+            bwClient.Publish(publishRequest, publishHandler);
 
-            if (!BWDefaults.DEFAULT_RESPONSEHANDLER(3).received)
-            {
-                Thread.SpinWait(10000000); // Wait before printing
-            }
-
-            Console.WriteLine(" ________________ \n|                |\n" + "|  status: " + BWDefaults.DEFAULT_RESPONSEHANDLER(1).result.status + "  |" + "\n|  set entity    |" + "\n|________________|");
-            Console.WriteLine(" ________________ \n|                |\n" + "|  status: " + BWDefaults.DEFAULT_RESPONSEHANDLER(2).result.status + "  |" + "\n|  subscribed    |" + "\n|________________|");
-            Console.WriteLine(" ________________ \n|                |\n" + "|  status: " + BWDefaults.DEFAULT_RESPONSEHANDLER(3).result.status + "  |" + "\n|  published     |" + "\n|________________|");
-            Console.WriteLine(" __________________________________________________________________\n|                                                                  |\n" + "|  uri: " + BWDefaults.DEFAULT_MESSAGEHANDLER(2).message.uri + "  |" + "\n|  from: " + BWDefaults.DEFAULT_MESSAGEHANDLER(2).message.from + "              |" + "\n|  type: " + BWDefaults.DEFAULT_MESSAGEHANDLER(2).message.payloadObjects[0].type.ToString() + "                                       |" + "\n|  message: " + Encoding.UTF8.GetString(BWDefaults.DEFAULT_MESSAGEHANDLER(2).message.payloadObjects[0].load) + "                                |" + "\n|__________________________________________________________________|");
+            semaphore.WaitOne();
+            Printer.PrintResponse(RequestType.MAKE_ENTITY, BWDefaults.DEFAULT_RESPONSEHANDLER(1).result.status);
+            Printer.PrintResponse(RequestType.SUBSCRIBE, subscribeHandler.result.status);
+            Printer.PrintResponse(RequestType.PUBLISH, publishHandler.result.status);
+            semaphore.WaitOne();
+            Printer.PrintMessage(nsgHandler.message);
 
             RequestUtils publishRequestUtils1 = new RequestUtils("stubbe.demo.dk/sharptesting", RequestType.PUBLISH)
                 .SetPrimaryAccessChain("klAVQ9UOajySAZZWzdU4IBrdyqs85JOk3TJhnpecFqg=");
-            string message1 = "That sucks, cause I could really use some sleep.";
+            string message1 = "Oh man, dawg! That sucks, cause I could really use some goddamn sleep right now. Ya' know, I have been awake for 43 hours? Oh man, dawg! That sucks, cause I could really use some goddamn sleep right now. Ya' know, I have been awake for 43 hours? Oh man, dawg! That sucks, cause I could really use some goddamn sleep right now. Ya' know, I have been awake for 43 hours?";
             byte[] text1 = { 64, 0, 0, 0 };
             publishRequestUtils1.AddPayloadObject(new PayloadObject(new PayloadType(text1), Encoding.UTF8.GetBytes(message1)));
             Request publishRequest1 = publishRequestUtils1.BuildPublisher();
-            bwClient.Publish(publishRequest1, BWDefaults.DEFAULT_RESPONSEHANDLER(4));
+            bwClient.Publish(publishRequest1, publishHandler1);
 
-            if (!BWDefaults.DEFAULT_RESPONSEHANDLER(4).received)
-            {
-                Thread.SpinWait(10000000); // Wait before printing
-            }
+            semaphore.WaitOne();
+            semaphore.WaitOne();
+            Printer.PrintMessage(nsgHandler.message);
 
-            Console.WriteLine(" __________________________________________________________________\n|                                                                  |\n" + "|  uri: " + BWDefaults.DEFAULT_MESSAGEHANDLER(2).message.uri + "  |" + "\n|  from: " + BWDefaults.DEFAULT_MESSAGEHANDLER(2).message.from + "              |" + "\n|  type: " + BWDefaults.DEFAULT_MESSAGEHANDLER(2).message.payloadObjects[0].type.ToString() + "                                       |" + "\n|  message: " + Encoding.UTF8.GetString(BWDefaults.DEFAULT_MESSAGEHANDLER(2).message.payloadObjects[0].load) + "       |" + "\n|__________________________________________________________________|");
 
             RequestUtils publishRequestUtils2 = new RequestUtils("stubbe.demo.dk/sharptesting", RequestType.PUBLISH)
                 .SetPrimaryAccessChain("klAVQ9UOajySAZZWzdU4IBrdyqs85JOk3TJhnpecFqg=");
@@ -88,17 +83,43 @@ namespace Bootstrap
             PayloadObject payload2 = new PayloadObject(new PayloadType(text2), message2);
             publishRequestUtils2.AddPayloadObject(payload2);
             Request publishRequest2 = publishRequestUtils2.BuildPublisher();
-            bwClient.Publish(publishRequest2, BWDefaults.DEFAULT_RESPONSEHANDLER(5));
+            bwClient.Publish(publishRequest2, publishHandler2);
 
-            if (!BWDefaults.DEFAULT_RESPONSEHANDLER(5).received)
-            {
-                Thread.SpinWait(10000000); // Wait before printing
-            }
-
-            Console.WriteLine(" __________________________________________________________________\n|                                                                  |\n" + "|  uri: " + BWDefaults.DEFAULT_MESSAGEHANDLER(2).message.uri + "  |" + "\n|  from: " + BWDefaults.DEFAULT_MESSAGEHANDLER(2).message.from + "              |" + "\n|  type: " + BWDefaults.DEFAULT_MESSAGEHANDLER(2).message.payloadObjects[0].type.ToString() + "                                       |" + "\n|  message: " + Encoding.UTF8.GetString(BWDefaults.DEFAULT_MESSAGEHANDLER(2).message.payloadObjects[0].load) + "                                 |" + "\n|__________________________________________________________________|");
+            semaphore.WaitOne();
+            semaphore.WaitOne();
+            Printer.PrintMessage(nsgHandler.message);
 
         }
-    }
 
+
+        class ResponseHandler : IResponseHandler
+        {
+            public Response result { get; set; }
+
+            public bool received { get; set; }
+
+            public void ResponseReceived(Response result)
+            {
+                this.result = result;
+                received = true;
+                semaphore.Release();
+            }
+        }
+
+        class MessageHandler : IMessageHandler
+        {
+            public Message message { get; set; }
+
+            public bool received { get; set; }
+
+            public void ResultReceived(Message message)
+            {
+                this.message = message;
+                received = true;
+                semaphore.Release();
+            }
+        }
+
+    }
 
 }
